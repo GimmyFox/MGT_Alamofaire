bf6d493342a449609857fdf77fd56b4060afe57f
GimmyFox <mguzeev99@gmail.com>
2022-05-25 18:09:28 +0300

    Initial Commit

---
+++ MGT_Alamofaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MGT_Alamofaire
+//
+//  Created by Maksim Guzeev on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MGT_AlamofaireTests/MGT_AlamofaireTests.swift
@@ -0,0 +1,33 @@
+//
+//  MGT_AlamofaireTests.swift
+//  MGT_AlamofaireTests
+//
+//  Created by Maksim Guzeev on 25.05.2022.
+//
+
+import XCTest
+@testable import MGT_Alamofaire
+
+class MGT_AlamofaireTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
